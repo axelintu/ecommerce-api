5cf133449d664f5bd98fefbc6e440e0db63c0c3e
Alejandro Flores <darklintumon@gmail.com>
2026-04-15 20:53:16 -0600

    Add missing functioanlity to artistController and bug fixes.

--- src/controllers/artistController.js
+++ src/controllers/artistController.js
@@ -1,49 +1,84 @@
 import Artist from "../models/Artist.js";
-export const getArtists = async (req, res) => {
+export const getArtists = async (req, res, next) => {
 	try {
 		const artists = await Artist.find();
 		res.status(200).json(artists)
 	} catch (error) {
-		console.error(error);
+		next(error);
 	}
 };
 
-export const getArtistById = async (req, res) => {
+export const getArtistById = async (req, res, next) => {
 	try {
 		const { id } = req.params;
-		const artist = Artist.findById(id);
+		const artist = await Artist.findById(id);
 		if (!artist) {
 			return res.status(404).json({ message: "Artist not found " });
 		}
 		res.status(200).json(artist);
 	} catch (error) {
-		console.error(error);
+		next(error);
 	}
 };
-export const createArtist = async (req, res) => {
+export const createArtist = async (req, res, next) => {
 	try {
-		const { urlName, name, description } = req.body;
+		const {
+			urlName,
+			name,
+			description,
+			imageURL,
+			backgroundImageURL
+		} = req.body;
 		const newArtist = await Artist.create({
 			urlName,
 			name,
-			description
+			description,
+			imageURL,
+			backgroundImageURL
 		});
 		res.status(201).json(newArtist);
 	} catch (error) {
-		console.error(error);
+		next(error);
 	}
 };
-export const updateArtist = async (req, res) => {
+export const updateArtist = async (req, res, next) => {
 	try {
 		const { id } = req.params;
+		const {
+			urlName,
+			name,
+			description,
+			imageURL,
+			backgroundImageURL
+		} = req.body;
+		const updatedArtist = await Artist.findByIdAndUpdate(
+			id,
+			{
+				urlName,
+				name,
+				description,
+				imageURL,
+				backgroundImageURL
+			},
+			{ new: true }
+		);
+		if (!updatedArtist) {
+			return res.status(404).json({ message: "Artist not found" });
+		}
+		res.status(200).json(updatedArtist);
 	} catch (error) {
-		console.error(error);
+		next(error);
 	}
 };
-export const deleteArtist = async (req, res) => {
+export const deleteArtist = async (req, res, next) => {
 	try {
 		const { id } = req.params;
+		const artist = await Artist.findByIdAndDelete(id);
+		if (!artist) {
+			return res.status(404).json({ message: "Artist not found" });
+		}
+		res.status(204).send();
 	} catch (error) {
-		console.error(error);
+		next(error);
 	}
 };
