22c53e6804b6dfcd15be8308c9da14ae531c127c
Alejandro Flores <darklintumon@gmail.com>
2026-04-19 12:59:04 -0600

    Add remaining controllers base files.
    - Category
    - Order
    - PaymentMethod
    - Product
    - Wishlist

---
+++ src/controllers/categoryController.js
@@ -0,0 +1,83 @@
+import Category from "../models/Category.js";
+
+export const getCategories = async (req, res, next) => {
+	try {
+		const categories = await Category.find().populate("parentCategory");
+		res.status(200).json(categories)
+	}
+	catch (error) { next(error); }
+};
+
+export const getCategoryById = async (req, res, next) => {
+	try {
+		const { id } = req.params;
+		const category = await Category.findById(id);
+		if (!category) {
+			return res.status(404).json({ message: "Category not found " });
+		}
+		res.status(200).json(category);
+	}
+	catch (error) { next(error); }
+};
+
+export const createCategory = async (req, res, next) => {
+	try {
+		const {
+			urlName,
+			name,
+			description,
+			imageURL,
+			backgroundImageURL
+		} = req.body;
+		const newCategory = await Category.create({
+			urlName,
+			name,
+			description,
+			imageURL,
+			backgroundImageURL
+		});
+		res.status(201).json(newCategory);
+	}
+	catch (error) { next(error); }
+};
+
+export const updateCategory = async (req, res, next) => {
+	try {
+		const { id } = req.params;
+		const {
+			urlName,
+			name,
+			description,
+			imageURL,
+			backgroundImageURL
+		} = req.body;
+		const updatedCategory = await Category.findByIdAndUpdate(
+			id,
+			{
+				urlName,
+				name,
+				description,
+				imageURL,
+				backgroundImageURL
+			},
+			{ new: true }
+		);
+		if (!updatedCategory) {
+			return res.status(404).json({ message: "Category not found" });
+		}
+		res.status(200).json(updatedCategory);
+	}
+	catch (error) { next(error); }
+};
+
+export const deleteCategory = async (req, res, next) => {
+	try {
+		const { id } = req.params;
+		const category = await Category.findByIdAndDelete(id);
+		if (!category) {
+			return res.status(404).json({ message: "Category not found" });
+		}
+		res.status(204).send();
+	}
+	catch (error) { next(error); }
+};
