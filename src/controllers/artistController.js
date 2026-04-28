714bd8e0a0a4cb3f559f1a8d37ec2d862cb22459
Alejandro Flores <darklintumon@gmail.com>
2026-04-28 13:42:35 -0600

    Update artistController for partial updates and  to prevent duplicate artist url.

--- src/controllers/artistController.js
+++ src/controllers/artistController.js
@@ -2,10 +2,10 @@ import Artist from "../models/Artist.js";
 
 export const getArtists = async (req, res, next) => {
 	/*  #swagger.tags = ['Artists']
-	*/
+	 */
 	try {
 		const artists = await Artist.find();
-		res.status(200).json(artists)
+		res.status(200).json(artists);
 	} catch (error) {
 		next(error);
 	}
@@ -13,7 +13,7 @@ export const getArtists = async (req, res, next) => {
 
 export const getArtistById = async (req, res, next) => {
 	/*  #swagger.tags = ['Artists']
-	*/
+	 */
 	try {
 		const { id } = req.params;
 		const artist = await Artist.findById(id);
@@ -28,54 +28,67 @@ export const getArtistById = async (req, res, next) => {
 
 export const createArtist = async (req, res, next) => {
 	/*  #swagger.tags = ['Artists']
-	*/
+	 */
 	try {
-		const {
+		const { 
 			urlName,
 			name,
 			description,
 			imageURL,
-			backgroundImageURL
+			backgroundImageURL,
 		} = req.body;
+		const existingArtist = await Artist.findOne({ urlName });
+		if (existingArtist) {
+			return res
+				.status(409)
+				.json({ message: "Artist with this URL name already exists" });
+		}
 		const newArtist = new Artist({
 			urlName,
 			name,
 			description,
 			imageURL,
-			backgroundImageURL
+			backgroundImageURL,
 		});
 		await newArtist.save();
-		res.status(201).json(newArtist);
+		const artistResponse = newArtist.toObject();
+		res.status(201).json({
+			_id: newArtist._id,
+			...artistResponse,
+		});
 	} catch (error) {
 		next(error);
 	}
 };
 export const updateArtist = async (req, res, next) => {
 	/*  #swagger.tags = ['Artists']
-	*/
+	 */
 	try {
 		const { id } = req.params;
-		const {
-			urlName,
-			name,
-			description,
-			imageURL,
-			backgroundImageURL
+		const { 
+			urlName, 
+			name, 
+			description, 
+			imageURL, 
+			backgroundImageURL 
 		} = req.body;
-		const updatedArtist = await Artist.findByIdAndUpdate(
-			id,
-			{
-				urlName,
-				name,
-				description,
-				imageURL,
-				backgroundImageURL
-			},
-			{ new: true }
-		);
-		if (!updatedArtist) {
+		const currentArtist = await Artist.findById(id);
+		if (!currentArtist) {
 			return res.status(404).json({ message: "Artist not found" });
 		}
+		const updateData = {};
+		if (urlName !== undefined) updateData.urlName = urlName;
+		if (name !== undefined) updateData.name = name;
+		if (description !== undefined) updateData.description = description;
+		if (imageURL !== undefined) updateData.imageURL = imageURL;
+		if (backgroundImageURL !== undefined)
+			updateData.backgroundImageURL = backgroundImageURL;
+
+		const updatedArtist = await Artist.findByIdAndUpdate(
+			id,
+			updateData,
+			{ new: true }
+		);
 		res.status(200).json(updatedArtist);
 	} catch (error) {
 		next(error);
@@ -83,7 +96,7 @@ export const updateArtist = async (req, res, next) => {
 };
 export const deleteArtist = async (req, res, next) => {
 	/*  #swagger.tags = ['Artists']
-	*/
+	 */
 	try {
 		const { id } = req.params;
 		const artist = await Artist.findByIdAndDelete(id);
